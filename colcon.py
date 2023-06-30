0a561806b807ecbfa68bdd48978b9d505277cc8a
Robin Rahm <robinrahm@hotmail.com>
2023-06-30 21:30:32 +0200

    small edit

--- colcon.py
+++ colcon.py
@@ -1,9 +1,11 @@
 def even(n):
-    return n*3+1
+    return n * 3 + 1
+
 
 def odd(n):
-    return n/2
+    return n / 2
 
-def isoddeven(n):
-    if n%2 != 0: return even(n)
+
+def is_odd_even(n):
+    if n % 2 != 0: return even(n)
     return odd(n)
--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import colcon as c
 from pprint import pprint
 
-result = []
-for i in range(100, 10000):
-    r = []
-    j = i
-    while j >= 2:
-        j = c.isoddeven(j)
-        r.append(j)
-    result.append(r)
-pprint(result)
+
+def main():
+    result = []
+    for i in range(100, 10000):
+        r = []
+        j = i
+        while j >= 2:
+            j = c.is_odd_even(j)
+            r.append(j)
+        result.append(r)
+    for res in result:
+        pprint(len(res))
+
+
+if __name__ == "__main__":
+    main()
