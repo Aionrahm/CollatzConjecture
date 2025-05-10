39e168b2ddbf3ef95f125a2d9bb95822f8f55a55
Robin Rahm <robinrahm@hotmail.com>
2025-05-10 22:14:09 +0200

    fixed odd and even

--- colcon.py
+++ colcon.py
@@ -1,11 +1,11 @@
-def even(n):
+def odd(n):
     return n * 3 + 1
 
 
-def odd(n):
+def even(n):
     return n / 2
 
 
 def is_odd_even(n):
-    if n % 2 != 0: return even(n)
+    if n % 2 == 0: return even(n)
     return odd(n)
--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import colcon as c
 from pprint import pprint
+import random
 
 
 def main():
+    result = []
+    j = random.randint(1, 1000000)
+    while j >= 2:
+        j = c.is_odd_even(j)
+        result.append(j)
+    print(result)
+    plotting(result)
+
+def multi():
     result = []
     for i in range(100, 10000):
         r = []
@@ -14,6 +24,12 @@ def main():
     for res in result:
         pprint(len(res))
 
+def plotting(r):
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    plt.plot(r)
+    plt.show()
 
 if __name__ == "__main__":
     main()
